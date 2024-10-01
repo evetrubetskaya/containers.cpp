6316ae0c4aa759b6881cb18ddcdedf9222a90338
evetrubetskaya <evtrubetskaya@yandex.ru>
2024-10-01 16:50:25 +0300

    refactor: move containers.h

---
+++ src/containers.h
@@ -0,0 +1,10 @@
+#ifndef S21_CONTAINERS_H
+#define S21_CONTAINERS_H
+
+#include "library/list.h"
+#include "library/queue.h"
+#include "library/stack.h"
+#include "library/vector.h"
+#include "library/array.h"
+
+#endif  // S21_CONTAINERS_H
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "../../containers.h"
+#include "../containers.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
